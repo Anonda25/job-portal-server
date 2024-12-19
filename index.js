66f619ca915336ef4dfbd2d35ae6931032dd0e44
Anonda25 <anondabiswas25@gmail.com>
2024-12-19 22:46:04 +0600

    this is the setup project

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 4000;
@@ -30,7 +31,12 @@ async function run() {
     const jobsApplyCollactions = client.db('jobhanter').collection('job-application')
 
     app.get('/job', async (req, res) => {
-      const cursor = jobCollactions.find()
+      const email = req.query.email;
+      let query = {};
+      if (email) {
+        query = { hr_email: email }
+      }
+      const cursor = jobCollactions.find(query)
       const result = await cursor.toArray()
       res.send(result)
     })
@@ -42,38 +48,77 @@ async function run() {
       const result = await jobCollactions.findOne(filter)
       res.send(result)
     })
+    // app.post include in the client site added data
+    app.post('/job', async (req, res) => {
+      const query = req.body;
+      const result = await jobCollactions.insertOne(query);
+      res.send(result)
+    })
 
     //job applycation list 
-    app.get('/job-applications', async (req, res)=>{
+    app.get('/job-applications', async (req, res) => {
       const email = req.query.email;
-      const query = { applycant_email: email}
+      const query = { applycant_email: email }
       const result = await jobsApplyCollactions.find(query).toArray()
 
-      for(const application of result){
-        console.log(application.job_id);
-        const query = {_id :  new ObjectId(application.job_id)}
+      for (const application of result) {
+        const query = { _id: new ObjectId(application.job_id) }
         const job = await jobCollactions.findOne(query);
-        if(job){
-            application.title = job.title,
+        
+        if (job) {
+          application.title = job.title,
             application.company = job.company
-            application.category = job.category
-            application.company_log = job.company_logo
-            application.description= job.description
-            application.location= job.location
-            application.requirements= job.requirements
+          application.category = job.category
+          application.company_logo = job.company_logo
+          application.description = job.description
+          application.location = job.location
+          application.requirements = job.requirements
           application.salaryRange = job.salaryRange
         }
       }
 
       res.send(result)
     })
+    app.get('/job-applications/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const result = await jobsApplyCollactions.findOne(filter)
+      res.send(result)
+    })
+
 
     app.post('/job-applications', async (req, res) => {
       const application = req.body;
       const result = await jobsApplyCollactions.insertOne(application)
+
+      //count methoude
+      const id = application.job_id;
+      const query = { _id: new ObjectId(id) };
+      const job = await jobCollactions.findOne(query)
+      console.log(job);
+      let newCount = 0;
+      if (job.applicationCount) {
+        newCount = job.applicationCount + 1;
+      } else {
+        newCount = 1;
+      }
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set:{
+          applicationCount : newCount
+        }
+      }
+      const updateResult = await jobCollactions.updateOne(filter, updateDoc)
       res.send(result)
     })
+    // delete my application jobs 
 
+    app.delete('/job-applications/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await jobsApplyCollactions.deleteOne(filter);
+      res.send(result)
+    })
 
 
 
